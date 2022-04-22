a970600bda721425b164fd44aa6eae3b2e2a8383
Jonathan <jonathan.mammozzetti@itssmartacademy.it>
2022-04-22 16:15:23 +0200

    Post e Delete tabella persona

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const express = require("express")
 const { getConnection } = require("./connection")
 const app = express()
+const bodyParser = require('body-parser')
 
-app.get("/", async (res, req) => {
+app.use(bodyParser.json())
+
+app.get("/", async (req, res) => {
     const connection = await getConnection();
     const [lista_persone] = await connection.query("SELECT * FROM Persona");
     console.log(lista_persone);
-    req.json(lista_persone).send();
+    res.json(lista_persone).send();
 });
 
-//app.post("/", (res, req) => {
-//    const connection = await getConnection();
-//    await connection.query("INSERT INTO Persona VALUES (2, Pippo, Moriarti, PIPMOR231");
-//})
+app.post("/", async (req, res) => {
+    const connection = await getConnection();
+    await connection.query("INSERT INTO persona (nome, cognome, codice_fiscale) VALUES (?, ?, ?);", [req.body.nome, req.body.cognome, req.body.codice_fiscale]);
+    console.log("ok");
+    res.json("ok").send();
+})
 
+app.delete("/", async (req, res) => {
+    const connection = await getConnection();
+    await connection.query("DELETE FROM persona WHERE id = ?", [req.body.elimina]);
+    console.log("ok");
+    res.json("ok").send();
+})
 
 app.listen(3000)
