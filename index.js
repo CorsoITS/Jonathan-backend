ca0d88fdca78620a286df10f430cd22023a87192
Jonathan <jonathan.mammozzetti@itssmartacademy.it>
2022-04-23 10:45:29 +0200

    Tentativo di organizzazione

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
 const express = require("express")
-const { getConnection } = require("./connection")
 const app = express()
-const bodyParser = require('body-parser')
 
-app.use(bodyParser.json())
 
-app.get("/", async (req, res) => {
-    const connection = await getConnection();
-    const [lista_persone] = await connection.query("SELECT * FROM Persona");
-    console.log(lista_persone);
-    res.json(lista_persone).send();
-});
-
-app.post("/", async (req, res) => {
-    const connection = await getConnection();
-    await connection.query("INSERT INTO persona (nome, cognome, codice_fiscale) VALUES (?, ?, ?);", [req.body.nome, req.body.cognome, req.body.codice_fiscale]);
-    console.log("ok");
-    res.json("ok").send();
-})
-
-app.delete("/", async (req, res) => {
-    const connection = await getConnection();
-    await connection.query("DELETE FROM persona WHERE id = ?", [req.body.elimina]);
-    console.log("ok");
-    res.json("ok").send();
-})
-
-app.listen(3000)
